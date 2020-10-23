85525ff43ad32d05b150cb4557e4ff44b048f2ab
prince-09 <52371330+prince-09@users.noreply.github.com>
2020-10-23 18:02:00 +0530

    Revert "Adding_UI_for_Login_Page (#2)" (#5)
    This reverts commit 925b6c2e5fb271acd2e6eb166c924272bb00766f.

--- app/src/main/java/com/dscnitp/touristshelperapp/MainActivity.java
+++ app/src/main/java/com/dscnitp/touristshelperapp/MainActivity.java
@@ -2,10 +2,7 @@ package com.dscnitp.touristshelperapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,18 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = (Button) findViewById(R.id.button_send);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(),Google_Sign_in.class);
-                view.getContext().startActivity(intent);
-            }
-
-
-            });
-
     }
-
 }
