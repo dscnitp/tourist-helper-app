c49a008d33675329d80830d1f7c9168eea8706f5
aarikasrivastava <57625428+aarikasrivastava@users.noreply.github.com>
2020-12-23 23:52:43 +0530

    api_integration_get_method_profilefragment (#21)

--- app/src/main/java/com/dscnitp/touristshelperapp/activity/EditProfile.java
+++ app/src/main/java/com/dscnitp/touristshelperapp/activity/EditProfile.java
@@ -14,5 +14,6 @@ public class EditProfile extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_profile);
+
     }
 }
